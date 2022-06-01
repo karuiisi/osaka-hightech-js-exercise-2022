6a625128949c99a830c675ed25491145cb783c55
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-01 11:32:42 +0900

    変更

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-for i in range(0,16){
-  const place = document.querySelector('.pos-1');
-  piece.style.order = parseInt(Math.random() * 16) + 1;
-}
-
-
 const piece01 = document.querySelector('.pos-1');
 piece01.style.order = parseInt(Math.random() * 16) + 1;
 

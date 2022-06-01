ddcf734fb02fef580bf087738c78cc4737b2c7cb
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-01 10:57:15 +0900

    変更

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
 const piece01 = document.querySelector('.pos-1');
 
 piece01.style.order = parseInt(Math.random() * 16) + 1;
-piece02.style.order = parseInt(Math.random() * 16) + 1;
-piece03.style.order = parseInt(Math.random() * 16) + 1;
-piece04.style.order = parseInt(Math.random() * 16) + 1;
-piece05.style.order = parseInt(Math.random() * 16) + 1;
-piece06.style.order = parseInt(Math.random() * 16) + 1;
-piece07.style.order = parseInt(Math.random() * 16) + 1;
-piece08.style.order = parseInt(Math.random() * 16) + 1;
-piece09.style.order = parseInt(Math.random() * 16) + 1;
-piece10.style.order = parseInt(Math.random() * 16) + 1;
-piece11.style.order = parseInt(Math.random() * 16) + 1;
-piece12.style.order = parseInt(Math.random() * 16) + 1;
-piece13.style.order = parseInt(Math.random() * 16) + 1;
-piece14.style.order = parseInt(Math.random() * 16) + 1;
-piece15.style.order = parseInt(Math.random() * 16) + 1;
-piece16.style.order = parseInt(Math.random() * 16) + 1;

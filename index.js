d544a68535fb8df297a2df81c9f7b8211abb6d1c
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-08 11:05:06 +0900

    hennkou

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 初期化処理 (全てのピースをランダムに配置する)
 for (let n = 1; n <= 16; n = n + 1) {
   const piece = document.querySelector('.pos-' + n);
 
@@ -5,9 +6,11 @@ for (let n = 1; n <= 16; n = n + 1) {
   piece.style.order = parseInt(Math.random() * 16) + 1;
 }
 
-function pieceClickhandler(){
+// ピースがクリックしたときに実行する処理 (関数)
+function pieceClickHandler() {
   console.log('ピースがクリックされました');
+}
 
-  const pice01 = document.querySelector('.pos-1')
+const piece01 = document.querySelector('.pos-1');
 
-  piece01.addEventListener('click',pieceClickHandler);
+piece01.addEventListener('click', pieceClickHandler);

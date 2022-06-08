ed038f4d5ea6cc82a9be4e48f56e6177310c9899
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-06-08 10:58:21 +0900

    変更

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ for (let n = 1; n <= 16; n = n + 1) {
   // MEMO: 1 ～ 16 でランダムに数値を決めると、番号が被る場合がある
   piece.style.order = parseInt(Math.random() * 16) + 1;
 }
+
+function pieceClickhandler(){
+  console.log('ピースがクリックされました');
+
+  const pice01 = document.querySelector('.pos-1')
+
+  piece01.addEventListener('click',pieceClickHandler);

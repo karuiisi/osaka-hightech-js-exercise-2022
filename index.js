0058df9a11725e758cdcafb5ba963a90d5cf8fcc
karuiisi <85538968+karuiisi@users.noreply.github.com>
2022-08-26 15:17:19 +0900

    変更(最初から)

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let up;    // 空白ピース基準で 1 つ上のピースの場所を記録
 let down;  // 空白ピース基準で 1 つ下のピースの場所を記録
 let left;  // 空白ピース基準で 1 つ左のピースの場所を記録
 let right; // 空白ピース基準で 1 つ右のピースの場所を記録
-
+let count=0;// クリック数を記録
 // 各ピースの場所を記録
 let positions = [
    6,  4,  3, 10,
@@ -12,66 +12,63 @@ let positions = [
    9, 13, 11,  8,
   15, 14, 12, 16,
 ];
-
-//シャッフル機能の実装
+//シャッフル関数
 function randomizePositions(array){
-  for(var i =(array.length - 1);0<i;i--){
-    var r = Math.floor(Math.random()*(i+1));
+  for(var i = (array.length - 1); 0 < i; i--){
+    var r = Math.floor(Math.random() * (i + 1));
     var tmp = array[i];
-    array[i]=array[r];
-    array[r]=tmp;
+    array[i] = array[r];
+    array[r] = tmp;
   }
   return array;
 }
-
 // 空白ピースを基準に、上下左右のピースの場所を調べる関数
 // ----------------------------------------------------------------------------
 function calcAdjacentPositions() {
   const empty = positions[15];
-
   let temp_up    = empty - 4;
   let temp_down  = empty + 4;
   let temp_left  = empty - 1;
   let temp_right = empty + 1;
-
   if (temp_up   <   1 ) temp_up    = null;
   if (temp_down >   16) temp_down  = null;
   if (empty % 4 === 1 ) temp_left  = null;
   if (empty % 4 === 0 ) temp_right = null;
-
   up    = temp_up;
   down  = temp_down;
   left  = temp_left;
   right = temp_right;
 }
-
-
 // Component 相当の関数を準備 (State => View にあたるもの)
 // ----------------------------------------------------------------------------
 function component() {
   for (let n = 0; n < 16; n = n + 1) {
     const piece = document.querySelector('.piece-' + (n + 1));
-
     piece.style.order = positions[n];
   }
-  document.getElementByld('count').innerHTML="現在の移動回数"+count;
+  document.getElementById('count').innerHTML = "現在の移動回数"+count ;
 }
-
-
 // 初期化処理
 // ----------------------------------------------------------------------------
 randomizePositions(positions);
 component();
 calcAdjacentPositions();
-
-
-
+//配列の要素の順番を確認する関数
+function isFinished(array){
+  for(var i = 0; (array.length - 1) > i; i++){
+    if (i+1!=array[i]){
+      return false;
+    }
+    if (array[i]==(array.length - 1)){
+      return true;
+    }
+  }
+}
 // ピースがクリックされたときに実行する処理 (関数)
 // ----------------------------------------------------------------------------
 function pieceClickHandler(event) {
   // event.target からピースの番号 N を特定する (文字で取得されるので数値に変換する)
   const N = Number(event.target.innerText);
-
   if (
     positions[N - 1] === up   ||
     positions[N - 1] === down ||
@@ -80,25 +77,21 @@ function pieceClickHandler(event) {
   ) {
     // ピースの場所を入れ替える
     [ positions[15], positions[N - 1] ] = [ positions[N - 1], positions[15] ];
-
     // State => View の反映を行う
     count++;
     component();
-
     // 隣接するピースを再計算する
-    calcAdjacentPositions(); 
+    calcAdjacentPositions();
   }
-//クリアメッセージの表示 alert関数を用いる
-if(isFinished(positions)==true){
-  window.alert("手数"+count+"congratulation"+"もう1回やる？");
-  document.location.reload();
- }
+  //クリアした時に，アラートを表示（OKを押すとリロード）
+  if(isFinished(positions)==true){
+    window.alert("手数"+count+"congratulation");
+    window.alert("Reset");
+    document.location.reload();
+  }
 }
-
 // 1 ～ 15 番ピースのクリックを監視し、クリックされたら pieceClickHandler を呼ぶ
 // ----------------------------------------------------------------------------
 for (let n = 1; n <= 15; n = n + 1) {
   const piece = document.querySelector('.piece-' + n);
-
   piece.addEventListener('click', pieceClickHandler);
-}
